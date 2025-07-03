9690eadc5f967e24f315b89025538094323dadeb
Eloon Ing <eing067@aucklanduni.ac.nz>
2025-07-03 13:13:50 +1200

    Added Utils and methods to read csv files. Added EmployeeDetails csv. Added method in main to read the external data.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 package src;
 
+import java.util.List;
+
+// The PAYE Table only contains data between $1100 and $1320
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+
+        List<String> PAYEdata = Utils.readPAYE();
+        List<String> EmployeeData = Utils.readEmployee();
+        System.out.println(EmployeeData);
     }
 }
---
+++ src/Utils.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Utils {
+    
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static List<String> readPAYE() {
+        return readCsv("./resources/PAYETable.csv");
+    }
+
+    public static List<String> readEmployee() {
+        return readCsv("./resources/EmployeeDetails.csv");
+    }
+
+    private static List<String> readCsv(String fileName) {
+        List<String> result = new LinkedList<>();
+
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while ((line = br.readLine()) != null) { //until the file has lines
+                result.add(line); 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    return result;
+    }
+}
