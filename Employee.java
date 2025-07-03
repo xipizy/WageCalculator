88b4c673499859232d7eb3ac7d3319ddc5344d11
Eloon Ing <eing067@aucklanduni.ac.nz>
2025-07-03 12:51:41 +1200

    set getters and constructor in employee class

---
+++ Employee.java
@@ -0,0 +1,29 @@
+public class Employee {
+    private String name;
+    private String PAYE;
+    private boolean studentLoan = false;
+    private int kiwiSaver;
+
+    public Employee(String name, String PAYE, boolean studentLoan, int kiwiSaver) {
+        this.name = name;
+        this.PAYE = PAYE;
+        this.studentLoan = studentLoan;
+        this.kiwiSaver = kiwiSaver;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPAYE() {
+        return PAYE;
+    }
+
+    public boolean isStudentLoan() {
+        return studentLoan;
+    }
+
+    public int getKiwiSaver() {
+        return kiwiSaver;
+    }   
+}
